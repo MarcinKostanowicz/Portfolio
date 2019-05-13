968559132da34df3d89409a8aa7ccbc236e9c0a4
MarcinKostanowicz <marcinkostanowiczgithub@wp.pl>
2019-05-13 19:09:40 +0200

    poprawa bledu

--- Portfolio/biblioteka1.cs
+++ Portfolio/biblioteka1.cs
@@ -6,21 +6,15 @@ using System.Threading.Tasks;
 
 namespace Portfolio
 {
-    class biblioteka1
+    class Biblioteka1
     {
-        static void Main(string[] args)
-        
-        {     //ZMIENNE STAŁE
-            byte zmienna = byte.MaxValue; //przypisuje najmniejsza wartosc
-            long zmienna8 = long.MaxValue;    // zmienne nie maja wartosci ujemnych
-            short zmienna1 = short.MaxValue; //posrednia wartosc
-            int zmienna7 = int.MaxValue;   //zmienna ma okreslona liczbe 
-            bool zmienna11 = 6 > 3; //true; //false  wartosc zwaracana
-            Console.WriteLine(zmienna11);
-            var zmienna22 = 1;
-            var zmienna2 = "blablab"; //typ string(tekstowy),liczbowy,logiczne
-          //Console.WriteLine ("Zespol ksiazek");
-            Console.ReadKey();
-        }
+        //ZMIENNE STAŁE
+        const byte zmienna = byte.MaxValue; //przypisuje najmniejsza wartosc
+        readonly long zmienna8 = long.MaxValue;    // zmienne nie maja wartosci ujemnych
+        short zmienna1 = short.MaxValue; //posrednia wartosc
+        int zmienna7 = int.MaxValue;   //zmienna ma okreslona liczbe 
+        bool zmienna11 = 6 > 3; //true; //false  wartosc zwaracana
+        string zmienna22 = "1";
+        string zmienna2 = "blablab"; //typ string(tekstowy),liczbowy,logiczne
     }
 }
