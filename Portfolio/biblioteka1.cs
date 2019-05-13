3649ab27507735bcf907708e23d60496b28e6348
unknown <marcinkostanowiczgithub@wp.pl>
2019-05-13 18:59:13 +0200

    dodanie pliku biblioteka + csproj

---
+++ Portfolio/biblioteka1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio
+{
+    class biblioteka1
+    {
+        static void Main(string[] args)
+        
+        {     //ZMIENNE STAŁE
+            byte zmienna = byte.MaxValue; //przypisuje najmniejsza wartosc
+            long zmienna8 = long.MaxValue;    // zmienne nie maja wartosci ujemnych
+            short zmienna1 = short.MaxValue; //posrednia wartosc
+            int zmienna7 = int.MaxValue;   //zmienna ma okreslona liczbe 
+            bool zmienna11 = 6 > 3; //true; //false  wartosc zwaracana
+            Console.WriteLine(zmienna11);
+            var zmienna22 = 1;
+            var zmienna2 = "blablab"; //typ string(tekstowy),liczbowy,logiczne
+          //Console.WriteLine ("Zespol ksiazek");
+            Console.ReadKey();
+        }
+    }
+}
