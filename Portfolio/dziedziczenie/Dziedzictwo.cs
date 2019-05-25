0cf66ce210178ba90caf46c4c858a91fda1c480f
unknown <marcinkostanowiczgithub@wp.pl>
2019-05-25 10:28:46 +0200

    folder dziedziczenie

---
+++ Portfolio/dziedziczenie/Dziedzictwo.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Portfolio.basic2
+{
+
+    public interface ILiga
+    {
+        string GetTabela();
+    }
+
+    public class Liga
+    {
+       
+
+        public virtual string GetTabela()
+        {
+            return "punktacja";
+
+        }
+    }
+
+    public class Legia : ILiga
+    {
+
+        public string GetTabela()
+        {
+            return "Legia";
+        }
+    }
+
+    public class Wisla : ILiga
+    {
+        
+        public string GetTabela()
+        {
+            return "Wisla";
+        }
+    }
+    
+    public class DziedziczenieTest2
+    {
+        [Test]
+        public void TestTabela()
+        {
+            ILiga liga = new Wisla();
+            liga.GetTabela();
+
+            ILiga legia = new Legia();
+            legia.GetTabela();
+
+
+        }
+            
+            
+
+            
+            
+
+
+
+            
+        
+
+
+
+
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+    
+    
+}
---
+++ Portfolio/dziedziczenie/Dziedziczenie.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Portfolio.basic2
+{
+    public class Samochod
+    {
+        public virtual string GetMarka()
+        {
+            return "Samochod zastepczy";
+        }
+    }
+
+    public class Audi : Samochod
+    {
+        public new string GetMarka()
+        {
+            return "Audi";
+        }
+    }
+
+    public class Bmw : Samochod
+    {
+        public override string GetMarka()
+        {
+            return "Bmw";
+        }
+    }
+    public class DziedziczenieTest
+    {
+        [Test]
+        public void TestMarek()
+        {
+            Samochod samochod = new Samochod();
+            samochod.GetMarka();
+
+            Samochod audi = new Audi();
+            audi.GetMarka();
+
+            Bmw bmw = new Bmw();
+            bmw.GetMarka();
+        }
+
+    }
+}
